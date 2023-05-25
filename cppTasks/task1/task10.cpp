92cdfa8ee6754bd82e267e23bb1ea99d066689de
Manal Ibrahim <133666612+manalimanali@users.noreply.github.com>
2023-05-25 19:23:52 +0300

    Add files via upload
    cpp tasks of robotic course

---
+++ cppTasks/task1/task10.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "HELLO WORLD";
+    return 0;
+}
---
+++ cppTasks/task1/task1_1*.cpp
@@ -0,0 +1,9 @@
+# 1 "/home/manali/roscourse/cppTasks/task1/task1_1*.cpp"
+# 1 "/home/manali/roscourse/cppTasks/task1//"
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "HELLO WORLD";
+    return 0;
+}
