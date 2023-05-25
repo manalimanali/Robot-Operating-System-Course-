63a529cae4300c2f37175c890ef34e833b1a19d3
Manal Ibrahim <133666612+manalimanali@users.noreply.github.com>
2023-05-25 21:11:48 +0300

    Add files via upload
    TASK1 INCLUDES 8 TASKS

---
+++ cppTasks/task1/task11/task12.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "\t\t\t subject \t\t\t marks\n math\t\t\t 90\ncomp\t\t\t 90\n chemist\t\t\t90\n";
+    return 0;
+}
---
+++ cppTasks/task1/task15/task1_5.cpp
@@ -0,0 +1,18 @@
+
+ #include <iostream>
+
+using namespace std;
+
+int main() {
+    float r,t,p,sir;
+    std::cout <<"wlcm to interest rate calculator (simple)\n";
+    std::cout <<"enter amount (principle)";
+    std:: cin>>p;
+   std::cout <<"\nenter time in years";
+   std:: cin>>t;
+    std::cout << "\nenter rate as a precentage";
+    std::cin>>r;
+    sir= p*(1+t*(r/100));
+    std:: cout<<" result = "<< sir;
+    return 0;
+}
