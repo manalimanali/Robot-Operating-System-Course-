aa934493e28ed8227eb0798d64ed860016419169
Manal Ibrahim <133666612+manalimanali@users.noreply.github.com>
2023-05-25 20:07:31 +0300

    Add files via upload
    print sum of 2 numbers after scanning them from user

---
+++ cppTasks/task13/task1_3.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1,num2,sum;
+    std::cout << "kindly.. welcm to summing pogram\nkindly enter first number\n";
+    std:: cin>> num1;
+    std:: cout <<"kindly.. enter second number\n";
+    std::cin>>num2;
+    sum=num1+num2;
+    std::cout<<"esult is ="<<sum;
+    return 0;
+}
